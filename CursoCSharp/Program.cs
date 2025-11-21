9080bedb334e149c1ff05878fec0c723c42d44f6
MiguelNeyoy <miguelneyoycn@gmail.com>
2025-11-21 14:59:03 -0700

    Se agrago la funciopnalidad de añadir tareas

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -5,7 +5,25 @@ namespace cursoCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] Tasks;
+            Console.Write("¿Cuántas tareas deseas ingresar? ");
+
+            int taskCount = int.Parse(Console.ReadLine());
+            Tasks = new string[taskCount];
+            for (int i = 0; i < taskCount; i++)
+            {
+                Console.Write($"Ingresa la tarea {i + 1}: ");
+                Tasks[i] = Console.ReadLine();
+            }
+
+            Console.Write("\n");
+            Console.Write("\n");
+            Console.Write("\n");
+            foreach (string task in Tasks)
+            {
+                Console.WriteLine($"Tarea: {task}");
+            }
+            Console.WriteLine("Presiona una tecla para salir...");
             Console.ReadKey();
         }
     }
--- CursoCSharp/obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
+++ CursoCSharp/obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bda3c2aa80b4cbf9572c1ce8bebb3a80faa8cc19")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
