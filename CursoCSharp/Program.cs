bda3c2aa80b4cbf9572c1ce8bebb3a80faa8cc19
MiguelNeyoy <miguelneyoycn@gmail.com>
2025-11-21 13:47:05 -0700

    Primer commit de este curso

---
+++ CursoCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace cursoCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
