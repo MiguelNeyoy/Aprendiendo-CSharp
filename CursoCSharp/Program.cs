cd83df6908a3e06b48caaa3bb6aeca370059ad50
MiguelNeyoy <miguelneyoycn@gmail.com>
2025-11-22 13:48:19 -0700

    Se añadio la logica para cerrar el programa al presionar scape  a demas de un lista para las tareas

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -5,26 +5,41 @@ namespace cursoCSharp
     {
         static void Main(string[] args)
         {
-            string[] Tasks;
-            Console.Write("¿Cuántas tareas deseas ingresar? ");
+            bool Finalizar = false;
+            List<string> Tasks = new List<string>();
+            while (Finalizar)
+            { 
+                for (int i = 0; i < taskCount; i++)
+                {
+                    Console.Write($"Ingresa la tarea {i + 1}: ");
+                    Tasks[i] = Console.ReadLine();
+                }
 
-            int taskCount = int.Parse(Console.ReadLine());
-            Tasks = new string[taskCount];
-            for (int i = 0; i < taskCount; i++)
-            {
-                Console.Write($"Ingresa la tarea {i + 1}: ");
-                Tasks[i] = Console.ReadLine();
-            }
+                Console.Write("\n");
+                Console.Write("\n");
+                Console.Write("\n");
+                if (Tasks.Count > 0)
+                {
+                    foreach (string task in Tasks)
+                    {
+                        Console.WriteLine($"Tarea: {task}");
+                    }
 
-            Console.Write("\n");
-            Console.Write("\n");
-            Console.Write("\n");
-            foreach (string task in Tasks)
-            {
-                Console.WriteLine($"Tarea: {task}");
+                }
+                else
+                {
+                    Console.WriteLine("No hay tareas");
+                }
+                var tecla = Console.ReadKey(true).Key;
+                if (tecla == ConsoleKey.Escape)
+                {
+                    Finalizar = false;
+                }
+
             }
-            Console.WriteLine("Presiona una tecla para salir...");
-            Console.ReadKey();
+            Console.WriteLine("Fin del programa");
         }
+
+
     }
 }
